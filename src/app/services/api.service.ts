ae0d9ff1f22a37d77747b206876f632f72a545fb
Gabriel Carneiro <gscarneiro22@gmail.com>
2023-03-13 01:21:55 -0300

    races list inital

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) {
+    var serverUrl : string = '';
+
+    function get(path: string, params:any): Observable<Object> {
+      return http.get(path).subscribe;
+    }
+
+  }
+}
